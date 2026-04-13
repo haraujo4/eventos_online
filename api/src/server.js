5f53ac3f6a87ed477b8557ea6536e9df34e734e4
helisson <htaraujo4@gmail.com>
2026-04-13 18:25:30 -0300

    dependencia

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const http = require('http');
 const { Server } = require('socket.io');
 const app = require('./app');
-require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
